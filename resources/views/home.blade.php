d215ca046f20d4639df3a333b03caa64c3847936
saff-it <saff_it@ymail.com>
2022-09-07 17:39:24 +0200

    main completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
     <div class="container">
         <div class="ms_bottom-main-bar">
                 <div>
-                    <img src="../../public/assets/img/buy-comics-digital-comics.png" alt="product img">
+                    <img src="assets/img/buy-comics-subscriptions.png" alt="product img">
                 </div>
                 <h3> DIGITAL COMICS </h3>
         </div>
