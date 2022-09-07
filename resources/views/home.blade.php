fb85011ad54636f9e9c75326667754cc523d3ba8
saff-it <saff_it@ymail.com>
2022-09-07 16:49:37 +0200

    main section populated, retrieving data from db

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.main')
+
+@section('main-content')
+
+<section class="ms_jumpo-img"></section>
+
+<section class="container-fluid ms_main-sect">
+
+    <div class="container ms_main-contents">
+
+        <div class="ms_dc-card">
+            <div class="ms_card-img">
+                <img src="singleCard.thumb" alt="singleCard.type">
+            </div>
+            <h5>singleCard.series</h5>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="container-fluid ms_bottom-main">
+
+    <div class="container">
+        <div class="ms_bottom-main-bar">
+            <div>
+                <img src="require" alt="product.text">
+
+            </div>
+            <h3> product.text </h3>
+
+        </div>
+    </div>
+
+</section>
+
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/app.css)}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
 
     <main>
         @yield('main-content')
-        
+
     </main>
     
 </body>
