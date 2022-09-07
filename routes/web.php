14a7770232a11579533721478b36093555a9206b
saff-it <saff_it@ymail.com>
2022-09-07 15:44:13 +0200

    databases imported and connected

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = config('comics');
+    return view('main', ['comics' => $data]);
 });
