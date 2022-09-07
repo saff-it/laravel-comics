7a2d43b886bd8619f35f0701e57090f4c3396b83
saff-it <saff_it@ymail.com>
2022-09-07 17:32:58 +0200

    bootstrap fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,12 @@
     <div class="container ms_main-contents">
 
         @foreach ($comics as $comic)
-        <div class="ms_dc-card">
-            <div class="ms_card-img">
-                <img src="{{$comic -> image}}">
+            <div class="ms_dc-card">
+                <div class="ms_card-img">
+                    <img src="{{$comic -> image}}">
+                </div>
+                <h5>{{$comic -> title}}</h5>
             </div>
-            <h5>{{$comic -> title}}</h5>
-        </div>
         @endforeach
     </div>
 
@@ -24,12 +24,10 @@
 
     <div class="container">
         <div class="ms_bottom-main-bar">
-            <div>
-                <img src="require" alt="product.text">
-
-            </div>
-            <h3> product.text </h3>
-
+                <div>
+                    <img src="../../public/assets/img/buy-comics-digital-comics.png" alt="product img">
+                </div>
+                <h3> DIGITAL COMICS </h3>
         </div>
     </div>
 
